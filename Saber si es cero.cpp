f2ef4ea4ae23002e2f433c9c817f94d49e62c3c3
JuanCruzManfredi <64174471+JuanCruzManfredi@users.noreply.github.com>
2021-06-12 21:22:36 -0300

    Add files via upload

---
+++ Saber si es cero.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>//llamada a las librerias 
+
+int result, num;
+
+main (){
+	
+	printf("\n  Ingrese numero:\n  ");
+	scanf("%d", &num);
+	
+  if (num == 0) {
+    printf("\n  Es cero\n");
+  }
+	
+	
+	
+	
+	
+	//result = num / 2 ;
+	//printf ("%f", result);
+	
+	
+}
